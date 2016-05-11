c7924acc88d25ec3d01efb50e8f454d73cade5a6
Jaemi Bremner <jaemibremner@Jaemis-MacBook-Pro.local>
2016-05-10 19:31:29 -0700

    Added Comment

---
+++ app.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+
+  //set variable that is equal to the score,  .html whatever is after it sets anything after that.  Line 11 and 12 set the score to 0.
+
+  var score=0;
+  $("#score").html(score);
+
+  //this second part is when click increase or decrease changes the main score section.
+
+  $(document).on("click", "#increase-5", function() {
+    score=score+5;
+    $("#score").html(score);
+  });
+
+  $(document).on("click", "#decrease-5", function() {
+    score=score-5;
+    $("#score").html(score);
+
+      if (score -5 >= 0) {
+          score = score -5;
+          // score += 5;
+      }
+  });
+
+  $(document).on("click", "#submit-custom-score", function() {
+    score=parseInt($("#custom-score").val());
+    $("#score").html(score);
+  });
+
+});
+//making simple change for github tutorial on may 10
